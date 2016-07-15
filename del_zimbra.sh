4b9bf529f2b096d850e445741716bb6ea3f75d49
tanvlt <tan.rmk@gmail.com>
2016-07-15 02:37:07 -0400

    delete mail zimbra following subject

---
+++ del_zimbra.sh
@@ -0,0 +1,16 @@
+#
+##List user mail####
+##acct=`zmprov -l gaa| grep tan`;
+
+acct=`cat /tmp/tanvl`
+for acct in ${acct}
+do
+
+##Get mail ID ########
+for msg in $(zmmailbox -z -m ${acct} s -l 7 -t message "From: networker@vietabank.com.vn Subject: REDMINE" | awk '{ print $2 }' | tail -n +6) ;
+do
+echo "Removing "$msg" from "$acct"";
+
+zmmailbox -z -m "${acct}" dm "${msg}";
+done
+done
